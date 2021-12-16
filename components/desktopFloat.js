bb565632c131fd4c5ca5d9927c4cf1e19bbeeaa8
Leon Rode <leon.rode13@gmail.com>
2021-12-15 22:21:26 -0500

    Add files via upload

--- components/desktopFloat.js
+++ components/desktopFloat.js
@@ -2,40 +2,32 @@ import style from "../styles/float.module.css";
 
 export default function DesktopFloat() {
   return (
-    <>
-      <div className={style.nameFloat}>
-        <h3 className={style.floatName}>
-          <span>
-            <a href="https://leonro.de">Leon Rode</a>
-          </span>{" "}
-          |{" "}
-        </h3>
-
-        <a href="https://github.com/leonrode/westchestercovidtracker.com">
-          <i
-            className="fab fa-github"
-            style={{ color: "#f6f6f6", scale: "1.2", marginLeft: "10px" }}
-          ></i>
-        </a>
-
-        <h3 className={style.floatName}>&nbsp; | </h3>
-        <a href="mailto:leon.rode13@gmail.com">
-          <i
-            className="fas fa-envelope"
-            style={{ color: "#f6f6f6", scale: "1.2", marginLeft: "10px" }}
-          ></i>
-        </a>
-      </div>
-      <div className={style.sourceFloat}>
-        <h3 className={style.sources}>
-          <span>Source: </span>
-          <a href="https://www.westchestergov.com">Westchester County</a> &{" "}
-          <a href="https://www.census.gov/">U.S. Census Bureau</a>
-        </h3>
-      </div>
+    <div className={style.wordsContainer}>
       <h5 className={style.disclaimer}>
         Data provided for reference only. No liability is assumed.
       </h5>
-    </>
+      <h5 className={style.source}>
+        <a href="https://www.westchestergov.com">Westchester County</a> &{" "}
+        <a href="https://www.census.gov/">U.S. Census Bureau.</a>
+      </h5>
+
+      <h5 className={style.author}>
+        <span>
+          <a href="https://leonro.de"> Leon Rode</a>
+        </span>
+        <a href="https://github.com/leonrode/westchestercovidtracker.com">
+          <i
+            className="fab fa-github"
+            style={{ color: "#666666", scale: "1.2", marginLeft: "7px" }}
+          ></i>
+        </a>
+        <a href="mailto:leon.rode13@gmail.com">
+          <i
+            className="fas fa-envelope"
+            style={{ color: "#666666", scale: "1.2", marginLeft: "10px" }}
+          ></i>
+        </a>
+      </h5>
+    </div>
   );
 }
